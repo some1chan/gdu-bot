19db4ec8870a885458f6a34dedceb91f8d863812
some1chan <31833556+some1chan@users.noreply.github.com>
2021-02-26 16:10:16 -0600

    feat: Suggest command supports edits better

--- src/plugins/Manage/commands/Suggest.ts
+++ src/plugins/Manage/commands/Suggest.ts
@@ -3,11 +3,13 @@ import {
 	BaseMessage,
 	BasePlugin,
 	Discord,
+	DiscordMessage,
 	EmbedHelper,
 	Logger,
-	Utils,
 } from "@framedjs/core";
-import { oneLine, stripIndent, stripIndents } from "common-tags";
+import { oneLine, stripIndents } from "common-tags";
+
+const msgUrlKey = "msgUrl";
 
 export default class extends BaseCommand {
 	suggestionChannelId = process.env.SUGGEST_CHANNEL_ID
@@ -28,12 +30,63 @@ export default class extends BaseCommand {
 		});
 	}
 
+	async findOldSuggestion(
+		msg: DiscordMessage,
+		suggestionChannel: Discord.TextChannel
+	): Promise<Discord.Message | undefined> {
+		let msgWithEmbed: Discord.Message | undefined;
+		if (msg.discord.msg?.edits) {
+			const collection = await suggestionChannel.messages.fetch({
+				around: msg.discord.msg.id,
+			});
+
+			const regex = /\[\]\(([^)]*)\)/g;
+			for (const [, suggestionMsg] of collection) {
+				const infoField = suggestionMsg.embeds[0]?.fields.find(
+					a => a.name == "Info"
+				);
+				if (infoField) {
+					// Attempts to find data in the info embed field
+					const matches = infoField.value.matchAll(regex);
+
+					for (const match of matches) {
+						const args = match[1]?.split(", ");
+						if (args && args[0] && args[1]) {
+							const msgUrl = args[0];
+							const key = args[1].replace(/"/g, "");
+
+							// If the key matches
+							if (key == msgUrlKey) {
+								// Check if the message ID in the URL matches this one
+								const msgUrlArgs = msgUrl.split("/");
+								const msgUrlValueId =
+									msgUrlArgs[msgUrlArgs.length - 1];
+
+								const thisUrlArgs = msg.discord.msg.url.split(
+									"/"
+								);
+								const thisUrlValueId =
+									thisUrlArgs[thisUrlArgs.length - 1];
+
+								if (msgUrlValueId == thisUrlValueId) {
+									msgWithEmbed = suggestionMsg;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return msgWithEmbed;
+	}
+
 	async run(msg: BaseMessage): Promise<boolean> {
 		if (msg.args && msg.args.length > 0) {
 			// Things
 			const argsContent = msg.getArgsContent();
 
-			if (msg.discord) {
+			if (msg instanceof DiscordMessage && msg.discord.msg) {
 				if (!msg.discord.guild) {
 					return false;
 				}
@@ -41,33 +94,38 @@ export default class extends BaseCommand {
 				const suggestionChannel = msg.discord.client.channels.cache.get(
 					this.suggestionChannelId
 				) as Discord.TextChannel;
+
+				let msgWithEmbed = await this.findOldSuggestion(
+					msg,
+					suggestionChannel
+				);
+
 				if (suggestionChannel) {
+					const data = `[](${msg.discord.msg.url}, "${msgUrlKey}")`;
 					const suggestionEmbed = EmbedHelper.getTemplate(
 						msg.discord,
 						await EmbedHelper.getCheckOutFooter(msg, this.id)
 					);
 					suggestionEmbed
-						.setTitle("Suggestion")
-						.addField(
-							"Author",
-							stripIndent`
-						${msg.discord.author} - \`User ID: ${msg.discord.author.id}\`
-						`
+						.setAuthor(
+							msg.discord.author.tag,
+							msg.discord.author.displayAvatarURL({})
 						)
-						.setDescription(
-							oneLine`Click **[here](https://discord.com/channels/${msg.discord.guild.id}/${msg.discord.channel.id}/${msg.discord.id})**
-							to see the original message. There may have been some edits!`
+						.addField(
+							"Info",
+							`${msg.discord.author} - [Click here](${msg.discord.msg.url}) to see the original message.${data}`
 						)
 						.addField("Suggestion", argsContent)
 						.setFooter(
-							`Suggestion created by ${
-								msg.discord.member
-									? `${msg.discord.member.displayName}#${msg.discord.author.discriminator}`
-									: msg.discord.author.tag
-							}`,
+							`User ID: ${msg.discord.author.id}`,
 							suggestionEmbed.footer?.iconURL
 						);
-					await suggestionChannel.send(suggestionEmbed);
+
+					if (msgWithEmbed) {
+						await msgWithEmbed.edit(suggestionEmbed);
+					} else {
+						await suggestionChannel.send(suggestionEmbed);
+					}
 
 					const thanks = stripIndents`
 					${msg.discord.author}, we appreciate your suggestion.
@@ -92,9 +150,11 @@ export default class extends BaseCommand {
 						Logger.error(error);
 					}
 
-					await Utils.sleep(5000);
 					try {
-						await thankMsg.delete();
+						await thankMsg.delete({
+							timeout: 5 * 1000,
+							reason: "Suggest command",
+						});
 					} catch (error) {
 						Logger.error(error.stack);
 					}
